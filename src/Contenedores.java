a44c191631809b1a5ca36a3188077b7c96c72aac
Le0-coder <leosan400@hotmail.com>
2022-09-11 20:18:52 -0600

    SE AGREGARON METODOS PARA BOTONES LAYOUT

--- src/Contenedores.java
+++ src/Contenedores.java
@@ -8,13 +8,78 @@ public class Contenedores extends JFrame{
 
     private JPanel panel1;
     private JButton salirButton;
-
+    private JButton borderLayoutButton;
+    private JLabel tittle;
+    private JButton gridLayoutButton;
+    private JButton flowLayoutButton;
+    private JButton gridBabLayoutButton;
+    private JButton cardLayoutButton;
 
 
     public Contenedores(){
         setContentPane(panel1);
 
-        //SETEAR ACCIONES AL BOTON SALIR
+        //SETEAR ACCIONES AL BOTON borderLayout
+
+        borderLayoutButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                //DESARROLLAR
+
+
+            }
+        });
+
+        //SETEAR ACCIONES AL BOTON gridLayout
+        gridLayoutButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                //DESARROLLAR
+
+
+            }
+        });
+
+        //SETEAR ACCIONES AL BOTON flowLayout
+        flowLayoutButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                //DESARROLLAR
+
+
+            }
+        });
+
+        //SETEAR ACCIONES AL BOTON gridBabLayout
+        gridBabLayoutButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                //DESARROLLAR
+
+
+            }
+        });
+
+        //SETEAR ACCIONES AL BOTON cardLayout
+        cardLayoutButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                //DESARROLLAR
+
+
+            }
+        });
+
+
+
+
+
+
         salirButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
