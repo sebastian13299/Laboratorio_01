7865260e97dc7008bfdd5cd5caef43e337238955
Le0-coder <leosan400@hotmail.com>
2022-09-11 19:56:15 -0600

    SE MODIFICARON CLASES BASE

--- src/Contenedores.java
+++ src/Contenedores.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class Contenedores {
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Contenedores extends JFrame{
+
+
+    private JPanel panel1;
+    private JButton salirButton;
+
+
+
+    public Contenedores(){
+        setContentPane(panel1);
+
+        //SETEAR ACCIONES AL BOTON SALIR
+        salirButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+                System.exit(0);
+            }
+        });
+
+
+
+
+
+    }
 }
