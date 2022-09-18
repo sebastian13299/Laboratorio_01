aaed094771ff15175f4fd824ae20d560265bc2be
JP Diaz <97.josepablods@gmail.com>
2022-09-17 19:13:42 -0600

    Add "actions" for flowlayout and gridbaglayout components

--- src/ExamplePack/FlowLayout.java
+++ src/ExamplePack/FlowLayout.java
@@ -3,6 +3,18 @@ package ExamplePack;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.List;
+
+ class  menuItemActionListener implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent event){
+        JDialog dialog = new JDialog(new JFrame("Option Dialog Frame"), "Menu Dialog Box");
+        JLabel label = new JLabel(String.format("Option '%s' chosen!", event.getActionCommand()));
+        dialog.add(label);
+        dialog.setSize(300, 200);
+        dialog.setVisible(true);
+    }
+};
 
 public class FlowLayout extends JFrame{
     private JTextField defaultTextFieldTextField;
@@ -10,6 +22,12 @@ public class FlowLayout extends JFrame{
     private JList list1;
     private JButton openJDialogButton;
     private JPanel mainFlowLayoutPanel;
+    private JButton showFieldTextButton;
+    private JButton showAreaTextButton;
+    private JButton showSelectedItemsButton;
+    private JLabel areaTextLabel;
+    private JLabel selectedItemsLabel;
+    private JLabel fieldTextLabel;
     private JFrame dialogFrame;
     private JMenuBar menuBar;
     private JMenu menu;
@@ -32,14 +50,41 @@ public class FlowLayout extends JFrame{
         openJDialogButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                dialogFrame = new JFrame("JDialog Frame");
-                dialogFrame.setVisible(true);
-                JDialog dialog = new JDialog(dialogFrame, "Dialog Box");
+                JDialog dialog = new JDialog(new JFrame("Dialog Frame"), "Dialog Box");
                 JLabel label = new JLabel("Default Dialog Box");
                 dialog.add(label);
                 dialog.setSize(300, 200);
                 dialog.setVisible(true);
             }
         });
+
+        showFieldTextButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                fieldTextLabel.setText(defaultTextFieldTextField.getText());
+            }
+        });
+
+        showAreaTextButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                areaTextLabel.setText(defaultTextAreaTextArea.getText());
+            }
+        });
+
+        showSelectedItemsButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                selectedItemsLabel.setText("");
+                List values = list1.getSelectedValuesList();
+                for (Object value : values) {
+                    selectedItemsLabel.setText(selectedItemsLabel.getText() + "\n" + (String) value);
+                }
+            }
+        });
+
+        menuItem1.addActionListener(new menuItemActionListener());
+        menuItem2.addActionListener(new menuItemActionListener());
+        menuItem3.addActionListener(new menuItemActionListener());
     }
 }
